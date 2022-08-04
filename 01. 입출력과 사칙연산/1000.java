686bffe6793be122ab15d5df2149b79031ed43a1
monki4746 <77190699+monki4746@users.noreply.github.com>
2022-08-04 19:03:55 +0900

    Update 1000.java

--- 01. 입출력과 사칙연산/1000.java
+++ 01. 입출력과 사칙연산/1000.java
@@ -1,1 +1,10 @@
-
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextInt();
+        b = scan.nextInt();
+        System.out.println(a + b);
+    }
+}
