74884c9c08a71941cd6aaffef37f7bcf8d2b6716
monki4746 <77190699+monki4746@users.noreply.github.com>
2022-08-04 19:10:16 +0900

    Update 1008.java

--- 01. 입출력과 사칙연산/1008.java
+++ 01. 입출력과 사칙연산/1008.java
@@ -1,1 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double a = scan.nextInt();
+        double b = scan.nextInt();
+        System.out.println(a / b);
+    }
+}
 
