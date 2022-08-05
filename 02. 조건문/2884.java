6ec4080b12e238d571734e3650a0cdbca1bc26be
monki4746 <77190699+monki4746@users.noreply.github.com>
2022-08-05 09:34:41 +0900

    Update 2884.java

--- 02. 조건문/2884.java
+++ 02. 조건문/2884.java
@@ -1,1 +1,23 @@
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) {
+        int H, M;
+        Scanner scan = new Scanner(System.in);
+        H = scan.nextInt();
+        M = scan.nextInt();
+        if ((M - 45) < 0) { 		
+            H--; 			
+            if (H < 0) { 		
+                H = 23;
+            }
+            M = 60 + (M - 45);
+        } else { 			
+            M -= 45; 			
+        }
+
+        System.out.println(H + " " + M);
+
+    }
+}
