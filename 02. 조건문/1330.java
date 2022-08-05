44a7031318fa53927a1dcf6ccef13089e26b3759
monki4746 <77190699+monki4746@users.noreply.github.com>
2022-08-05 09:30:29 +0900

    Update 1330.java

--- 02. 조건문/1330.java
+++ 02. 조건문/1330.java
@@ -1,1 +1,18 @@
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) {
+        int a, b;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextInt();
+        b = scan.nextInt();
+        if (a < b) {
+            System.out.println("<");
+        } else if (a > b) {
+            System.out.println(">");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
