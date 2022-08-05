b9d0d6cae0cbad7abb6f462ea2afa3a9080adc15
monki4746 <77190699+monki4746@users.noreply.github.com>
2022-08-05 09:34:01 +0900

    Update 14681.java

--- 02. 조건문/14681.java
+++ 02. 조건문/14681.java
@@ -1,1 +1,21 @@
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) {
+        int x, y;
+        Scanner scan = new Scanner(System.in);
+        x = scan.nextInt();
+        y = scan.nextInt();
+        if (x > 0 && y > 0) {
+            System.out.println(1);
+        } else if (x < 0 && y > 0) {
+            System.out.println(2);
+        } else if (x < 0 && y < 0) {
+            System.out.println(3);
+        } else if (x > 0 && y < 0) {
+            System.out.println(4);
+        }
+
+    }
+}
