61c405ff723b73e1cde5544fcece60f0840c5a07
monki4746 <77190699+monki4746@users.noreply.github.com>
2022-08-05 09:33:11 +0900

    Update 2753.java

--- 02. 조건문/2753.java
+++ 02. 조건문/2753.java
@@ -1,1 +1,21 @@
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) {
+        int year, bool = 0;
+        Scanner scan = new Scanner(System.in);
+        year = scan.nextInt();
+        if ((year % 4) == 0) {
+            bool = 1;
+        }
+        if ((year % 100) == 0) {
+            bool = 0;
+        }
+        if ((year % 400) == 0) {
+            bool = 1;
+        }
+        System.out.println(bool);
+
+    }
+}
