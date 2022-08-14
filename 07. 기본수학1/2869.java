b13824f7fd382451dc88483b6cef7509f1ad19e6
monki4746 <77190699+monki4746@users.noreply.github.com>
2022-08-14 23:38:07 +0900

    Update 2869.java

--- 07. 기본수학1/2869.java
+++ 07. 기본수학1/2869.java
@@ -1,1 +1,29 @@
+import java.util.*;
+import java.io.*;
 
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+		long V = Long.parseLong(st.nextToken());
+
+		long destination = V - B;
+		long dist = A - B;
+
+		long div = destination / dist;
+		long mod = destination % dist;
+
+		long day = div;
+		if(mod != 0) {
+			day = day + 1;
+			System.out.println(day);
+		}
+		else {
+			System.out.println(day);
+		}
+
+	}
+}
