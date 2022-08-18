388b4b48206505f443d79d1c6062ec5288c42daa
monki4746 <77190699+monki4746@users.noreply.github.com>
2022-08-18 23:40:33 +0900

    Update 2588.java

--- 01. 입출력과 사칙연산/2588.java
+++ 01. 입출력과 사칙연산/2588.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+
+		int a = in.nextInt();
+		int b = in.nextInt();
+		
+		int b1 = b / 100; 	
+		int b2 = b / 10 % 10;	
+		int b3 = b % 10;	
+		
+		System.out.println(a*b3);
+		System.out.println(a*b2);
+		System.out.println(a*b1);
+		System.out.println(a*b);
+		
+		in.close();
+	}
+}
 
