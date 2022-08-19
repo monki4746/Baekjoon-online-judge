5e92c020e0604d6e373a46e2ba7efeefb012e92e
monki4746 <77190699+monki4746@users.noreply.github.com>
2022-08-19 23:46:28 +0900

    Update 3003.java

--- 01. 입출력과 사칙연산/3003.java
+++ 01. 입출력과 사칙연산/3003.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int king = 1 - sc.nextInt();
+		int queen = 1 - sc.nextInt();
+		int rook = 2 - sc.nextInt();
+		int bishop = 2 - sc.nextInt();
+		int night = 2 - sc.nextInt();
+		int pawn= 8 - sc.nextInt();
+		
+		
+		sc.close();
+		
+		System.out.println(king + " " + queen + " "+ rook + " "+ bishop + " " + night + " "+ pawn );
+		
+	}
+}
